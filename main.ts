2691e1b9d51d370bb4f7d4264cad46345419fb5a
HuangRuoLei <1694001042@qq.com>
2020-08-19 11:35:33 +0800

    S

--- main.ts
+++ main.ts
@@ -42,12 +42,6 @@ namespace TuoYuCar {
         y,
         z
     }
-    export enum DisplayChance{
-        //% blockId="Chine" block="显示中文"
-        Chine = 0,
-        //% blockId="English"" block="显示字母""
-        English=1
-    }
     function IICWrite(value:number,value1:number) {
         
         pins.i2cWriteNumber(value, value1, NumberFormat.UInt8LE);
@@ -105,25 +99,40 @@ namespace TuoYuCar {
      * 选择以打开或关闭小车显示屏功能
      * @param index
     */
-    //% blockId=TuoYuCar_OLED block="显示屏|index %index|index1 %index1|index2 %index2"
+    //% blockId=TuoYuCar_OLED block="显示屏显示中文|index %index|index1 %index1"
     //% weight=97
     //% blockGap=10
     //% color="#006400"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
-    export function OLED(index:DisplayChance,index1:DisplayChine,index2:DisplayEnglish) {
+    export function OLEDShowChine(index:ultrasonicState,index1:DisplayChine) {
         switch (index) {
-            case DisplayChance.Chine: {
+            case ultrasonicState.Off: IICWrite(4,0); break;
+            case ultrasonicState.Open: {
                 switch (index1) {
                     case DisplayChine.wo: IICWrite(4, 1); break;
                     case DisplayChine.ni: IICWrite(4, 2); break;
                 }
-            } break;
-            case DisplayChance.English: {
-                switch (index2) {
+            }; break;
+        }
+    }
+        /**
+     * 选择以打开或关闭小车显示屏功能
+     * @param index
+    */
+    //% blockId=TuoYuCar_OLED block="显示屏显示字母|index %index|index1 %index1"
+    //% weight=97
+    //% blockGap=10
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function OLED(index:ultrasonicState,index1:DisplayEnglish) {
+        switch (index) {
+            case ultrasonicState.Off: IICWrite(5, 0); break;
+            case ultrasonicState.Open: {
+                switch (index1) {
                     case DisplayEnglish.a: IICWrite(5, 1); break;
-                    case DisplayEnglish.b: break;
-                    case DisplayEnglish.c: break;
-                    case DisplayEnglish.d: break;
+                    case DisplayEnglish.b: IICWrite(5, 2); break;
+                    case DisplayEnglish.c: IICWrite(5, 3); break;
+                    case DisplayEnglish.d: IICWrite(5, 4); break;
                 }
             }; break;
         }
