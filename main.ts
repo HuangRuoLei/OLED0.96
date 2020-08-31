7285ad7a179d3b6538aa64ef8dc59013a2c1a534
HuangRuoLei <1694001042@qq.com>
2020-08-31 17:08:27 +0800

    A

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% color="#006400" weight=20 icon="\uf1b9" block="拓宇小车"
+//% color="#006400" weight=2 0 icon="\uf1b9" block="拓宇小车"
 namespace TuoYuCar {
 
     export enum ultrasonicState{
@@ -77,6 +77,15 @@ namespace TuoYuCar {
         
         pins.i2cWriteNumber(value, value1, NumberFormat.UInt8LE);
     }
+    function IICWriteBuf(value: number, value1: number, value2: number, value3: number, value4: number) {
+        let buf = pins.createBuffer(4);
+        buf[0] = value1;
+        buf[1] = value2;
+        buf[2] = value3;
+        buf[3] = value4;
+        
+        pins.i2cWriteBuffer(value, buf);
+    }
     function SPIWrite(value: number) {
         pins.spiPins(DigitalPin.P0, DigitalPin.P1, DigitalPin.P2);
         pins.spiFormat(8, 3);
@@ -95,8 +104,8 @@ namespace TuoYuCar {
     export function Chao_Sheng_Bo(index: ultrasonicState):void {
         
         switch (index) {
-            case ultrasonicState.Off: IICWrite(1, 1); break;
-            case ultrasonicState.Open: IICWrite(1, 2); break;
+            case ultrasonicState.Off: IICWrite(65, 1); break;
+            case ultrasonicState.Open: IICWrite(65, 2); break;
         }
     }
 
@@ -111,7 +120,7 @@ namespace TuoYuCar {
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
     export function Read_Chao_Sheng_Bo(): number {
         let length;
-        length=pins.i2cReadNumber(1, NumberFormat.Int8LE);
+        length=pins.i2cReadNumber(65, NumberFormat.Int8LE);
         return length;
     }
     /**
@@ -126,8 +135,8 @@ namespace TuoYuCar {
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
     export function Yan_She_Chuan_Gan_Qi(index:ultrasonicState):void {
         switch (index) {
-            case ultrasonicState.Off: IICWrite(2, 1); break;
-            case ultrasonicState.Open: IICWrite(2, 2); break;
+            case ultrasonicState.Off: IICWrite(67, 1); break;
+            case ultrasonicState.Open: IICWrite(67, 2); break;
         }
     }
 
@@ -144,8 +153,8 @@ namespace TuoYuCar {
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
     export function Sheng_Ying_Chuan_Gan_Qi(index:ultrasonicState):void {
         switch (index) {
-            case ultrasonicState.Off: IICWrite(3, 1); break;
-            case ultrasonicState.Open: IICWrite(3, 2); break;
+            case ultrasonicState.Off: IICWrite(68, 1); break;
+            case ultrasonicState.Open: IICWrite(68, 2); break;
         }
     }
     /**
@@ -175,36 +184,38 @@ namespace TuoYuCar {
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=20
     export function OLEDShowChine(index:ultrasonicState,index2:x,index3:y,index1:DisplayChine):void {
         switch (index) {
-            case ultrasonicState.Off: IICWrite(4,0); break;
+            case ultrasonicState.Off: IICWriteBuf(69,1,0,0,0); break;
             case ultrasonicState.Open: {
+                let buf1 = pins.createBuffer(3);
                 switch (index2) {
-                    case x.Zero: IICWrite(6, 1); break;
-                    case x.Two: IICWrite(6, 2); break;
-                    case x.four: IICWrite(6, 3); break;
-                    case x.Six: IICWrite(6, 4); break;
+                    case x.Zero: buf1[0]=1; break;
+                    case x.Two: buf1[0]=2; break;
+                    case x.four: buf1[0]=3; break;
+                    case x.Six: buf1[0]=4; break;
                 }
                 switch (index3) {
-                    case y._0: IICWrite(6, 5); break;
-                    case y._8: IICWrite(6, 6); break;
-                    case y._16: IICWrite(6, 7); break;
-                    case y._24: IICWrite(6, 8); break;
-                    case y._32: IICWrite(6, 9); break;
-                    case y._40: IICWrite(6, 10); break;
-                    case y._48: IICWrite(6, 11); break;
-                    case y._56: IICWrite(6, 12); break;
-                    case y._64: IICWrite(6, 13); break;
-                    case y._72: IICWrite(6, 14); break;
-                    case y._80: IICWrite(6, 15); break;
-                    case y._88: IICWrite(6, 16); break;
-                    case y._96: IICWrite(6, 17); break;
-                    case y._104: IICWrite(6, 18); break;
-                    case y._112: IICWrite(6, 19); break;
+                    case y._0:  buf1[1]=1; break;
+                    case y._8:  buf1[1]=2; break;
+                    case y._16: buf1[1]=3; break;
+                    case y._24: buf1[1]=4; break;
+                    case y._32: buf1[1]=5; break;
+                    case y._40: buf1[1]=6; break;
+                    case y._48: buf1[1]=7; break;
+                    case y._56: buf1[1]=8; break;
+                    case y._64: buf1[1]=9; break;
+                    case y._72: buf1[1]=10; break;
+                    case y._80: buf1[1]=11; break;
+                    case y._88: buf1[1]=12; break;
+                    case y._96: buf1[1]=13; break;
+                    case y._104: buf1[1]=14; break;
+                    case y._112: buf1[1]=15; break;
                 }
 
                 switch (index1) {
-                    case DisplayChine.wo: IICWrite(4, 1); break;
-                    case DisplayChine.ni: IICWrite(4, 2); break;
+                    case DisplayChine.wo: buf1[2]=1; break;
+                    case DisplayChine.ni: buf1[2]=2; break;
                 }
+                IICWriteBuf(69, 2, buf1[0], buf1[1], buf1[2]);
             }; break;
         }
     }
@@ -219,60 +230,40 @@ namespace TuoYuCar {
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
     export function OLEDShowEnglish(index:ultrasonicState,index1:x,index2:y,index3:DisplayEnglish):void {
         switch (index) {
-            case ultrasonicState.Off: IICWrite(5, 0); break;
+            case ultrasonicState.Off: IICWriteBuf(70,1,0,0,0); break;
             case ultrasonicState.Open: {
+                let buf1 = pins.createBuffer(3);
                 switch (index1) {
-                    case x.Zero: IICWrite(6, 1); break;
-                    case x.Two: IICWrite(6, 2); break;
-                    case x.four: IICWrite(6, 3); break;
-                    case x.Six: IICWrite(6, 4); break;
+                    case x.Zero: buf1[0]=1; break;
+                    case x.Two: buf1[0]=2; break;
+                    case x.four: buf1[0]=3; break;
+                    case x.Six: buf1[0]=4; break;
                 }
                 switch (index2) {
-                    case y._0: IICWrite(6, 5); break;
-                    case y._8: IICWrite(6, 6); break;
-                    case y._16: IICWrite(6, 7); break;
-                    case y._24: IICWrite(6, 8); break;
-                    case y._32: IICWrite(6, 9); break;
-                    case y._40: IICWrite(6, 10); break;
-                    case y._48: IICWrite(6, 11); break;
-                    case y._56: IICWrite(6, 12); break;
-                    case y._64: IICWrite(6, 13); break;
-                    case y._72: IICWrite(6, 14); break;
-                    case y._80: IICWrite(6, 15); break;
-                    case y._88: IICWrite(6, 16); break;
-                    case y._96: IICWrite(6, 17); break;
-                    case y._104: IICWrite(6, 18); break;
-                    case y._112: IICWrite(6, 19); break;
-                    case y._120: IICWrite(6, 20); break;
+                    case y._0:  buf1[1]=1; break;
+                    case y._8:  buf1[1]=2; break;
+                    case y._16: buf1[1]=3; break;
+                    case y._24: buf1[1]=4; break;
+                    case y._32: buf1[1]=5; break;
+                    case y._40: buf1[1]=6; break;
+                    case y._48: buf1[1]=7; break;
+                    case y._56: buf1[1]=8; break;
+                    case y._64: buf1[1]=9; break;
+                    case y._72: buf1[1]=10; break;
+                    case y._80: buf1[1]=11; break;
+                    case y._88: buf1[1]=12; break;
+                    case y._96: buf1[1]=13; break;
+                    case y._104: buf1[1]=14; break;
+                    case y._112: buf1[1]=15; break;
+                    case y._120: buf1[1]=16; break;
                 }
                 switch (index3) {
-                    case DisplayEnglish.a: IICWrite(5, 1); break;
-                    case DisplayEnglish.b: IICWrite(5, 2); break;
-                    case DisplayEnglish.c: IICWrite(5, 3); break;
-                    case DisplayEnglish.d: IICWrite(5, 4); break;
+                    case DisplayEnglish.a: buf1[2]=1; break;
+                    case DisplayEnglish.b: buf1[2]=2; break;
+                    case DisplayEnglish.c: buf1[2]=3; break;
+                    case DisplayEnglish.d: buf1[2]=4; break;
                 }
-            }; break;
-        }
-    }
-      /**
-     * 选择以打开
-     * @param index
-    */
-    //% blockId=TuoYuCar_OLEDShowCce block="显示中|%index|横坐标 %index2"
-    //% weight=90
-    //% blockGap=10
-    //% color="#006400"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
-    export function OLEDShowCce(index:ultrasonicState,index2:x):void {
-        switch (index) {
-            case ultrasonicState.Off: IICWrite(4,0); break;
-            case ultrasonicState.Open: {
-                switch (index2) {
-                    case x.Zero: IICWrite(6, 1); break;
-                    case x.Two: IICWrite(6, 2); break;
-                    case x.four: IICWrite(6, 3); break;
-                    case x.Six: IICWrite(6, 4); break;
-                }
+                IICWriteBuf(70, 2, buf1[0], buf1[1], buf1[2]);
             }; break;
         }
     }
