272746bca2e0d79304c1e48c807bc6f0a58392ec
HuangRuoLei <69152711+HuangRuoLei@users.noreply.github.com>
2020-09-28 23:00:21 +0800

    A

--- main.ts
+++ main.ts
@@ -839,7 +839,7 @@ namespace TuoYuCar2{
      * @param index
     */
 
-    //% blockId=TuoYuCar2_Car_Rotation_angle block="控制小车|%index|旋转角度为 %speed °"
+    //% blockId=TuoYuCar2_Car_Rotation_angle block="控制小车|%index|旋转角度为 %speed "
     //% weight=99
     //% blockGap=10
     //% speed.min=0 speed.max=360
@@ -849,8 +849,11 @@ namespace TuoYuCar2{
         let buf1 = pins.createBuffer(3);
         basic.pause(10);
         if(speed>255)
-        buf1[1]=speed>>8;
-        buf1[2]=speed;
+        buf1[1]=360-255;
+        if(speed>255)
+            buf1[2]=255;
+        else 
+            buf1[2]=speed;
         switch (index) {
           case Drive1.turn_left:buf1[0]=1;break;
           case Drive1.turn_right:buf1[0]=2;break;
