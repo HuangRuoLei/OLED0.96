163b62bda98b09ac76eb8020f712ccc8573955c9
HuangRuoLei <69152711+HuangRuoLei@users.noreply.github.com>
2020-10-14 00:17:54 +0800

    A

--- main.ts
+++ main.ts
@@ -721,7 +721,7 @@ namespace TuoYuCar1{
         }
        // buf=index1;
         pins.i2cWriteBuffer(71,buf1);
-        basic.pause(buf1[0]);
+        basic.pause(index1);
         pins.i2cWriteNumber(71, index1, NumberFormat.UInt16LE);
     }
 
